894a58af6256b93ec47cdbe1f1b94fe27358088e
SubairMohamed <subairmohamed44@gmail.com>
2022-04-03 23:54:58 +0300

    js_basics_two

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ const z = 14;
 console.log(x + y + z);
 var iskugayn = x + y + z; 
 console.log(iskugayn);
+// laws in varaibles
+    // inaanad isticmaali karin javascript keywords
+    // waa inuu noqdaa meningfull
+    // waa inaanad ku bilaawin number
+    // waa inaanad space u dhaxiisiin
+     
+  
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+      
+
+
+
+
+
+
+
+
+   
+    
