cd3b57b338185d6a5e40ad42b63c6cd2e55e37c2
subair44 <subairmohamed44@gmail.com>
2022-04-04 23:45:28 +0300

    js-basics-three

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 // variables 
-var x = 10;
-let y = 12;
-const z = 14;
-console.log(x + y + z);
-var iskugayn = x + y + z; 
-console.log(iskugayn);
+
+// var x = 10;
+// let y = 12;
+// const z = 14;
+// console.log(x + y + z);
+// var iskugayn = x + y + z;
+// console.log(iskugayn);
+
 // laws in varaibles
-    // inaanad isticmaali karin javascript keywords
-    // waa inuu noqdaa meningfull
-    // waa inaanad ku bilaawin number
-    // waa inaanad space u dhaxiisiin
-     
-  
-   
+// inaanad isticmaali karin javascript keywords
+// waa inuu noqdaa meningfull
+// waa inaanad ku bilaawin number
+// waa inaanad space u dhaxiisiin
+
+// Javascript Operators
+// 01 Arithmetic operator
+// +  Addition
+//  - Substraction
+//  * Multiplication
+//  / Division
+//  % Modulus (Remainder)
+// ** Exponentiation  (waa square oo kale ) 
+// ++ Increment
+// -- Decrement
+
+// let dec = 10;
+// console.log(dec--);
+// console.log(dec);
+
+let inc = 100;
+console.log(inc++);
+console.log(inc);
 
 
 
@@ -37,9 +55,6 @@ console.log(iskugayn);
 
 
 
-   
-
-      
 
 
 
@@ -48,5 +63,8 @@ console.log(iskugayn);
 
 
 
-   
-    
+
+
+
+
+
