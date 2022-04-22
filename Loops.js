dce5a7ab98ef6c0d21dcd29afd397eee1f5f4883
subair44 <subairmohamed44@gmail.com>
2022-04-22 23:13:46 +0300

    hey-Loop

---
+++ Loops.js
@@ -0,0 +1,21 @@
+// Loops 
+// ========
+
+// 01 For 
+// 02 For..of
+// 03 For..in 
+// 04 While
+// 05 Do..while
+// 06 ForEach... HOF
+// --------------------
+
+// 01 For 
+
+// console.log("Loop1");
+// console.log("Loop2");
+// console.log("Loop3");
+// console.log("Loop4");
+// console.log("Loop5");
+
+
+
