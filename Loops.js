2672e1287133ecbfd271fc9ebfd889fa94ab3fd1
subair44 <subairmohamed44@gmail.com>
2022-04-23 06:04:05 +0300

    heyya-Loops

--- Loops.js
+++ Loops.js
@@ -17,5 +17,28 @@
 // console.log("Loop4");
 // console.log("Loop5");
 
+//i=5;i>0;i--
+//const names = ["subeer", "maxamed", "cali", "harbi", "ismaaciil"];
 
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+//     //if (i == 7) break;
+// }
 
+// 02 For..of
+
+// for (alpha of names) {
+//     console.log(alpha);
+// }
+
+// 03 For..in
+
+// const friends = {
+//     firstName: "subeer",
+//     lastName: "maxamed",
+//     surName: "Harbi",
+// };
+
+// for (kings in friends) {
+//     console.log(friends[kings]);
+// }
