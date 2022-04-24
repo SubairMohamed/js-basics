20c85f9c046aec57798464f002cf088d9de96fa8
SubairMohamed <subairmohamed44@gmail.com>
2022-04-24 23:58:53 +0300

    hey-switch-1

---
+++ switchStatement.js
@@ -0,0 +1,32 @@
+// switch statement 
+// ================
+
+let days = "fasax";
+
+switch (days) {
+    case "my days":
+    
+   case "khamiis":
+       console.log("waxaan kacaa 11:30 AM");
+       break;
+    case "jimci":
+        console.log("waxaan kacaa 12:30 PM");
+        break;
+    case "sabti":
+        console.log("waxaan kacaa sabtida 5:00 AM");
+        break;
+    case "axad":
+        console.log("waxaan kacaa axada 5:00 AM");
+        break;
+    case "isliin":
+        console.log("waxaan kacaa isliinta 5:00 AM");
+        break;
+    case "salaasa":
+        console.log("waxaan kacaa salaasada 5:00 AM");
+        break;
+    case "arbaca":
+        console.log("waxaan kacaa arbacada 5:00 AM");
+        break; 
+}
+
+// console.log(days);
