0d0d2455f2fca4d96176e77fa9d3d3231b3aeb8b
SubairMohamed <subairmohamed44@gmail.com>
2022-05-19 22:25:15 +0300

    hey-somcoder-1

--- SomCoder/index.js
+++ SomCoder/index.js
@@ -1,8 +1,29 @@
 // Template Literal
 // ================
 
-var magac = "faarax";
-var magacKale = "magacaygu waa " + magac;
+// var magac = "faarax";
 
-var magacCusub = "magacaygu waa $ {magac}";
-console.log(magacCusub);
+// var magacKale = "magacaygu waa " + magac+  " wuxu jiraa 20 sano";
+
+// var magacCusub = `waa kuma  ${magac} waxa iga yar laba caruura`;
+
+// console.log(magacCusub);
+// console.log(magacKale);
+// -----------------------------------------------------------------------
+
+// Type Conversion or Coersion
+// ===========================
+
+// String
+// let tiro = String(20);
+// // tiro = String(tiro);
+
+// let nooc = typeof tiro;
+// console.log(tiro);
+
+// Number 
+
+let tiro = Number ("20") + Number ("2");
+let nooc = typeof tiro;
+
+console.log(`${tiro}, ${nooc}`);
