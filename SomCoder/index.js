cb7b34bca84829e031e1a30ba6c74b466b4a51ca
SubairMohamed <subairmohamed44@gmail.com>
2022-05-20 14:34:38 +0300

    hey-comcoder-1-end

--- SomCoder/index.js
+++ SomCoder/index.js
@@ -22,12 +22,21 @@
 // console.log(tiro);
 
 // Number 
-
 // Number ("10") + Number ("2") waa isku mid  +"10" +  +"2"
 // Number ("10") + "2" waa isku mid +"10" + 2
 // Number(true) waa isku mid +true
 
-let tiro = +false + 4;
-let nooc = typeof tiro;
+// let tiro = +false + 4;
+// let nooc = typeof tiro;
+// console.log(`${tiro}, ${nooc}`);
 
-console.log(`${tiro}, ${nooc}`);
+// Data => Boolean
+// ==============
+
+// "", '', false, 0, -0, null, undefined, NaN => false
+
+// let tiro = Boolean ();
+// let nooc = typeof tiro;
+// console.log(`${tiro}, ${nooc}`);
+// if () console.log("qeemo ayaa ku dhex jira"); 
+
