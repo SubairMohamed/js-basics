e263641b97c2abb0c4d4ee524af3fbc70e0b1abf
subair44 <subairmohamed44@gmail.com>
2022-05-23 23:47:20 +0300

    hey-end-classes-understandable

--- SomCoder/classes.js
+++ SomCoder/classes.js
@@ -8,33 +8,65 @@
 // ---------
 // ES6 before 
 
-var User = function (name, email) {
-    this.name = name;
-    this.email = email;
+// var User = function (name, email) {
+//     this.name = name;
+//     this.email = email;
+// }
+
+// User.prototype.information = function () {
+//     return "Name: " + this.name + "Email: " + this.email;
+// }
+
+// const Mohamed = new User("Mohamed ", " Mohamed44@gmail.com");
+
+// console.log(Mohamed.information());
+
+// // extend 
+
+// const Admin = function (name, email, role) {
+//     User.call(this, name, email);
+//     this.role = role;
+// }
+
+// Object.getPrototypeOf(Admin, User.prototype); // extend
+
+// Admin.prototype.AdminInformation = function () {
+//     return "Name: " + this.name + " Email: " + this.email + " Role: " + this.role;
+// }
+
+// const subeer = new Admin("Subeer", "Subeer44@gmail.com", "Admin");
+
+// console.log(subeer.AdminInformation());
+// -----------------------------------------------------------------------
+
+// Classes
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    Information() {
+        return `name:${this.name} email:${this.email} `
+    }
 }
 
-User.prototype.information = function () {
-    return "Name: " + this.name + "Email: " + this.email;
+let Faarax = new User("faarax", "faarax44@yahoo.com");
+
+console.log(Faarax.Information());
+
+// extend
+
+class Admin extends User {
+    constructor(name, email, role) {
+        super(name, email,);
+        this.role = role;
+    }
+    AdminInformation() {
+        return `Name:${this.name} Email:${this.email} Role:${this.role}`
+    }
 }
 
-const Mohamed = new User("Mohamed ", " Mohamed44@gmail.com");
-
-console.log(Mohamed.information());
-
-// extend 
-
-const Admin = function (name, email, role) {
-    User.call(this, name, email);
-    this.role = role;
-}
-
-Object.getPrototypeOf(Admin, User.prototype); // extend
-
-Admin.prototype.AdminInformation = function () {
-    return "Name: " + this.name + " Email: " + this.email + " Role: " + this.role;
-}
-
-const subeer = new Admin("Subeer", "Subeer44@gmail.com", "Admin");
+let subeer = new Admin("subeer", "subeer44@gamil.com", "Admin");
 
 console.log(subeer.AdminInformation());
-
