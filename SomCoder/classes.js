206a6aa01c0333a6f772f4a3286d07888716834d
subair44 <subairmohamed44@gmail.com>
2022-05-23 21:46:27 +0300

    hey-understand-my-prototype

--- SomCoder/classes.js
+++ SomCoder/classes.js
@@ -20,3 +20,21 @@ User.prototype.information = function () {
 const Mohamed = new User("Mohamed ", " Mohamed44@gmail.com");
 
 console.log(Mohamed.information());
+
+// extend 
+
+const Admin = function (name, email, role) {
+    User.call(this, name, email);
+    this.role = role;
+}
+
+Object.getPrototypeOf(Admin, User.prototype); // extend
+
+Admin.prototype.AdminInformation = function () {
+    return "Name: " + this.name + " Email: " + this.email + " Role: " + this.role;
+}
+
+const subeer = new Admin("Subeer", "Subeer44@gmail.com", "Admin");
+
+console.log(subeer.AdminInformation());
+
