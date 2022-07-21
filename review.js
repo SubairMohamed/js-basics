cd32009e5de566c18c5d20b937f586622a3e56a6
subair44 <subairmohamed44@gmail.com>
2022-07-21 21:14:37 +0300

    understand-functions

--- review.js
+++ review.js
@@ -1,20 +1,33 @@
 // ES5 
- 
+
 // function sayHello (name, greet) {
 //     console.log("Hello " + name + " How are you ? " + greet );
 // }
 
 
-function sayHello (name) {
+function sayHello(name) {
     return "Hello " + name;
 }
 
 const jamal = sayHello("waa ina kaygi");
 
-// function 3 types are 
-// string 
+// function 3 types are
+// string
 // number
 // variable
+
+function multiby4(value) {
+    return value * 4;
+}
+
+function Hello(name) {
+    return "iska waran " + name;
+}
+
+const mo = " subeer maxamed ali xarbi";
+
+
+
 
 
 // ES6
@@ -25,3 +38,19 @@ const jamal = sayHello("waa ina kaygi");
 
 // namearr();
 
+// celsius to fahrenheit
+// radius of circle
+// circumference of circle
+
+
+// °F = °C×(9/5)+32
+
+const celTofah = (cel) => {
+    return cel * (9 / 5) + 32;
+}
+
+// r = c / (2 * π)
+
+const radiOFcir = (c) => {
+    return c / (2 * 3.14);
+} 
