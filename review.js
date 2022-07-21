d61141ccb1af9924120a47e701e6c91f4ca1b6cf
SubairMohamed <subairmohamed44@gmail.com>
2022-07-21 14:54:36 +0300

    understand-function

--- review.js
+++ review.js
@@ -1,7 +1,27 @@
 // ES5 
+ 
+// function sayHello (name, greet) {
+//     console.log("Hello " + name + " How are you ? " + greet );
+// }
 
-function Name (x, y) {
 
-    console.log("this is parameter one: ", xy, "this is parameter two: ", yf);
-
+function sayHello (name) {
+    return "Hello " + name;
 }
+
+const jamal = sayHello("waa ina kaygi");
+
+// function 3 types are 
+// string 
+// number
+// variable
+
+
+// ES6
+
+// const namearr = () => {
+//     console.log("you start arrow function ");
+// }
+
+// namearr();
+
