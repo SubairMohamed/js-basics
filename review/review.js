ac36842278577f1efe97abf67adc1ad5c8a2693c
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-06 13:40:32 +0300

    well

--- review/review.js
+++ review/review.js
@@ -1,5 +1,6 @@
 // ES5 BEFORE 2015
-function name(first, last) {
-  console.log("Hello " + first + " " + last);
+function name(x, y) {
+  console.log("parameter 1 is ", x, "parameter 2 is ", y);
 }
-name(subair);
+
+name(200, 300);
