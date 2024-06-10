dc880868f9bddfe3934687149eef635880a80451
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-10 22:06:33 +0300

    well

---
+++ review/pratices-pasenger.js
@@ -0,0 +1,26 @@
+const rakaab = [
+  {
+    name: "maxamed cali faraax",
+    amount: 5,
+    price: 10,
+  },
+  {
+    name: "suhayb yaasiin cabdi",
+    amount: 8,
+    price: 10,
+  },
+  {
+    name: "xamda nuur cali",
+    amount: 10,
+    price: 10,
+  },
+];
+
+// 01 magacada rakaabka ?
+// 02 rakaabka ay lactu ku dhiman tahay ?
+// 03 rakaabka lacagta wada bixiyay ?
+// 04 dhamaan lacagta ay bixiyiin rakaabka ?
+// 05 dhamaan lacagta laga doonayay ?
+// 06 dhamaan lacagta dhiman ?
+// 07 rakaab walba lacgta ku dhiman iyo magaciisa ?
+// ---------------------------------------------------
