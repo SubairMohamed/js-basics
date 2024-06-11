a47a665b55e3890b4cd3088929fdbf63d27d445a
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-11 22:25:09 +0300

    well

--- review/loop.js
+++ review/loop.js
@@ -1,1 +1,21 @@
-console.log("loop one");
+// Loops in javascript
+// ===================
+
+// 1) For loop
+// 2) For...of loop
+// 3) For...in loop
+// 4) ForEach loop = HOF
+// 5) While loop
+// 6) Do.While loop
+
+// 1) For loop
+
+// console.log("loops 1");
+// console.log("loops 2");
+// console.log("loops 3");
+// console.log("loops 4");
+// console.log("loops 5");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
