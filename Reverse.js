cbfdba69792014667448c02d0ac222586499165e
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-11 21:02:43 +0300

    well

--- Reverse.js
+++ Reverse.js
@@ -1,15 +1,15 @@
 // Reverse
 // ========
 // 1
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-numbers.reverse();
-console.log(numbers);
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+// numbers.reverse();
+// console.log(numbers);
 // 2
-const greenfruits = ["Apple", "Banana", "Mango", "Grapes"];
-greenfruits.reverse();
-console.log(greenfruits);
+// const greenfruits = ["Apple", "Banana", "Mango", "Grapes"];
+// greenfruits.reverse();
+// console.log(greenfruits);
 
 // 3
-const names = "the mern stack courses";
-const kalabadal = names.split("").reverse().join("");
-console.log(kalabadal);
+// const names = "the mern stack courses";
+// const kalabadal = names.split("").reverse().join("");
+// console.log(kalabadal);
