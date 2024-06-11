b57eb7b85103253bd7eec8d786ce68d2c1f17896
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-11 15:07:41 +0300

    well

--- Reverse.js
+++ Reverse.js
@@ -1,16 +1,15 @@
 // Reverse
 // ========
 // 1
-const numbers =[1,2,3,4,5,6,7];
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 numbers.reverse();
-// console.log(numbers);
+console.log(numbers);
 // 2
-const greenfruits = ["Apple", "Banana", "Mango", "Grapes",];
+const greenfruits = ["Apple", "Banana", "Mango", "Grapes"];
 greenfruits.reverse();
-// console.log(greenfruits);
+console.log(greenfruits);
 
-// 3 
+// 3
 const names = "the mern stack courses";
 const kalabadal = names.split("").reverse().join("");
-// console.log(kalabadal);
-
+console.log(kalabadal);
--- sort.js
+++ sort.js
@@ -1,8 +1,7 @@
 // Sort HOF
 // ========
-// sort ascending order.
+// sort ascending / alphabetic order.
 
 // const even = ["p", "a", "c", "e", "d", "f", "b"];
 // const odd = even.sort();
 // console.log(odd);
-
