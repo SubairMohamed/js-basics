b5a530fc883b50b59eb30fc2e958600ff261e543
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-19 21:52:20 +0300

    still

--- Data Structure Algorithm/validateSubsequence.js
+++ Data Structure Algorithm/validateSubsequence.js
@@ -1,13 +1,13 @@
 const array = [5, 1, 22, 25, 6, -1, 8, 10, 11, 7];
 const subsquence = [1, 6, -1, 10];
 
-function isValidSubsequence(array, subsquence) {
-  let arrIdx = 0;
-  let seqIdx = 0;
-  while (arrIdx < array.length && seqIdx < subsquence.length) {
-    if (seqIdx === subsquence.length) break;
-    if (subsquence[seqIdx] === array[arrIdx]) seqIdx++;
-    arrIdx++;
-  }
-  return seqIdx === subsquence.length;
-}
+// function isValidSubsequence(array, subsquence) {
+//   let arrIdx = 0;
+//   let seqIdx = 0;
+//   while (arrIdx < array.length && seqIdx < subsquence.length) {
+//     if (seqIdx === subsquence.length) break;
+//     if (subsquence[seqIdx] === array[arrIdx]) seqIdx++;
+//     arrIdx++;
+//   }
+//   return seqIdx === subsquence.length;
+// }
