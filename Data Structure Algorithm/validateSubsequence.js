8f2cdd342304129babcc6f7ebbcf5e71bdb08091
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-21 22:34:21 +0300

    well

--- Data Structure Algorithm/validateSubsequence.js
+++ Data Structure Algorithm/validateSubsequence.js
@@ -31,10 +31,14 @@ const competition = [{ Html: "C#" }, { "C#": "Js" }, { Js: "Html" }];
 
 let results = [0, 0, 1];
 
+const HOME_TEAM_WON = 1;
 function TournamentWinner(competition, results) {
   let currentBestTeam = "";
   const scores = { [currentBestTeam]: 0 };
   for (let idx = 0; idx < competition.length; idx++) {
     const result = results[idx];
+    const [homeTeam, awayTeam] = competition[idx];
+    const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+    updateScores(winningTeam, scores);
   }
 }
