75dd7f35f7916b61c4760be66642a0c9c7a8403b
SubairMohamed <SubairMohamed44@gmail.com>
2024-07-03 21:47:48 +0300

    still

---
+++ review/simple-challenge.js
@@ -0,0 +1,50 @@
+// 01 Total Quantity In Shop ?
+// 02 Total Items In Shop ?
+// 03 Items Quantity Above Twenty ?
+// 04 Total Price of All Quantity In Shop ?
+// 05 Total Price of Available Items ?
+// 06 Items Available In Shop ?
+// 07 Items Unavailable In Shop ?
+
+const shop = [
+  {
+    item: "Kaluun",
+    price: 9000,
+    quantity: 10,
+  },
+  {
+    item: "Mongo Sharaab",
+    price: 5000,
+    quantity: 8,
+  },
+  {
+    item: "Caano Maraaci",
+    price: 5000,
+    quantity: 20,
+  },
+  {
+    item: "Caano Lis",
+    price: 6000,
+    quantity: 24,
+  },
+  {
+    item: "Fanta",
+    price: 3500,
+    quantity: 50,
+  },
+  {
+    item: "Buskud Cake",
+    price: 1000,
+    quantity: 0,
+  },
+  {
+    item: "Saliid",
+    price: 80000,
+    quantity: 1,
+  },
+  {
+    item: "Jawaan Sonkor",
+    price: 120000,
+    quantity: 0,
+  },
+];
