11b8fb516caddc10115b0783c1fe33e11df489d6
SubairMohamed <SubairMohamed44@gmail.com>
2024-12-22 22:45:24 +0100

    stil

--- review/Advanced-HOF.js
+++ review/Advanced-HOF.js
@@ -4,3 +4,14 @@
 // 1) callback
 // ===========
 
+const back = (n) => {
+  return n ** 2;
+}
+
+console.log(back(2));
+
+const cube = (back, n) => {
+   return back(n) * n;
+}
+
+console.log(cube(back, 3));
