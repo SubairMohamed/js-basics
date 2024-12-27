609dc824c04dc1452ec0b9db1a761590ff96042b
SubairMohamed <SubairMohamed44@gmail.com>
2024-12-27 22:42:52 +0100

    WELL

---
+++ Advanced-Javascript/data-type.js
@@ -0,0 +1,12 @@
+// ADVANCED JAVASCRIPT 
+
+// DATA TYPES 
+//   1) Primitive Data Types
+//   2) NON-Primitive Data Types
+
+//  1) Primitive Data Types
+// Numbers - Integers, floats
+// Strings - Any data under single quote, double quote or backtick quote
+// Booleans - true or false value
+// Null - empty value or no value
+// Undefined - a declared variable without a value
