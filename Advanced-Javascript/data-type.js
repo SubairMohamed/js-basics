8933dc5babf0a5e45dec1554a3ad660175f400fb
SubairMohamed <SubairMohamed44@gmail.com>
2024-12-27 23:07:57 +0100

    well

--- Advanced-Javascript/data-type.js
+++ Advanced-Javascript/data-type.js
@@ -1,7 +1,7 @@
 // ADVANCED JAVASCRIPT 
 
 // DATA TYPES 
-//   1) Primitive Data Types
+//   1) Primitive Data Types immutable
 //   2) NON-Primitive Data Types
 
 //  1) Primitive Data Types
@@ -16,3 +16,21 @@
 let word = "nlpha";
 word[0] = "a";
 console.log(word);
+
+let num1 = 3;
+let num2 = 3;
+console.log(num1 == num2);
+
+// 2) NON-Primitive Data Types
+// Array = mutable
+// Object = mutable
+
+// Array
+
+let arr = [1, 2, 3, 4, 5];
+arr[0] = 85
+console.log(arr);
+
+let num = [6, 7, 8, 9];
+let numbers = [6, 7, 8, 9];
+console.log(num == numbers);
