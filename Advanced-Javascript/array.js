41a45b3894de50a7ff3ad5e2fe2f0cd31f6823be
SubairMohamed <SubairMohamed44@gmail.com>
2025-02-13 11:12:12 +0100

    stil

---
+++ Advanced-Javascript/array.js
@@ -0,0 +1,29 @@
+// Arrays
+  // =======
+  // Using Array constructor
+// const arr1 = Array();
+// console.log(arr1);
+
+// or
+// let arr2 = new Array();
+// console.log(arr2);
+
+  // Using square brackets []
+// This syntax the most recommended way to create an empty list
+// let arr3 = [];
+// console.log(arr3);
+
+ // Array with initial values
+ let fruits = ["apple", "banana", "mango", "orange"];
+ console.log(fruits.length);
+ 
+  // Array can have items of different data types
+ let diff = [
+   true, 
+   5, 
+   "name", 
+   null,
+   {university: "is a apha university and unity university"},
+   {skils: ["web deverloper"]}
+ ];
+ console.log(diff[4]);
